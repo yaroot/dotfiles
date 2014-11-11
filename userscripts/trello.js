7b1a971ce1621cf642aee630be60b64e5451c394
toni <tsu@yaroot.net>
2014-11-11 22:00:51 +0800

    .

---
+++ userscripts/trello.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        trello
+// @namespace   bg-trello
+// @include     https://trello.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+
+$(function() {
+  setInterval(function() {
+    $('body')[0].style.backgroundColor = '#068'
+    // console.log("setting text")
+  }, 2000)
+})
