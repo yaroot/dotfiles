73e602037d77039c9c7dd1e125aa561a9cae0107
yan <tsu@yaroot.net>
2023-12-07 15:21:31 +0900

    .

---
+++ nix-macos/flake.nix
@@ -0,0 +1,35 @@
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    unstable.url = "nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv/latest";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs, unstable, devenv, fenix }: {
+    packages."aarch64-darwin".default = let
+      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+      unstablePkgs = unstable.legacyPackages."aarch64-darwin";
+    in pkgs.buildEnv {
+      name = "home-packages";
+      paths = with pkgs; [
+
+        git
+        tmux
+        jq
+        silver-searcher
+        ripgrep
+        zstd
+        bat
+        htop
+
+      ];
+    };
+  };
+
+}
